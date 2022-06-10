05105e59915712c77e1aff91a68980be968bc61c
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-10 14:48:03 +0300

    commit tree 06baaf3d5efcc4bc77818e535c20bfa56b0d421b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(name, string) {
+    return name.filter(
+      (selectedMatching) => selectedMatching.toLowerCase() === string.toLowerCase()
+    );
+}
+
+function fuzzyMatch(name, string2) {
+    return name.filter(
+      (selectedMatching) =>
+      selectedMatching.toLowerCase().indexOf(string2.toLowerCase()) === 0
+    );
+}  
+
+function matchName(name, string3) {
+    return name.filter((info) => info.name === string3);
+}
